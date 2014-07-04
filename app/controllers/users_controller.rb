4a2f482eb6aa00ee12361d549d8d9b0174ac546f
Luka Novsak <lnovsak@gmail.com>
2014-07-04 20:57:24 +0200

    fix indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,24 +6,24 @@ class UsersController < ApplicationController
   end
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-   @ppdow_q = params[:ppdow_q].try(:to_sym) || :all_time
-   @pphod_q = params[:pphod_q].try(:to_sym) || :all_time
-   
-   @ppdow_gt = q_to_date(@ppdow_q)
-   @pphod_gt = q_to_date(@pphod_q)
+    @ppdow_q = params[:ppdow_q].try(:to_sym) || :all_time
+    @pphod_q = params[:pphod_q].try(:to_sym) || :all_time
+    
+    @ppdow_gt = q_to_date(@ppdow_q)
+    @pphod_gt = q_to_date(@pphod_q)
 
-   @ppdow_posts = if @ppdow_gt then @user.posts.
-                      where(Post.arel_table[:remote_created_at].gt(@ppdow_gt))
-                  else @user.posts end
-   @pphod_posts = if @pphod_gt then @user.posts.
-                      where(Post.arel_table[:remote_created_at].gt(@pphod_gt))
-                  else @user.posts end
+    @ppdow_posts = if @ppdow_gt then @user.posts.
+                       where(Post.arel_table[:remote_created_at].gt(@ppdow_gt))
+                   else @user.posts end
+    @pphod_posts = if @pphod_gt then @user.posts.
+                       where(Post.arel_table[:remote_created_at].gt(@pphod_gt))
+                   else @user.posts end
 
-   @posts_per_dow = @ppdow_posts.group_by_day_of_week(:remote_created_at).count.
-     map { |k,v| [dow_to_name(k), v] }
-   @posts_per_hod = @pphod_posts.group_by_hour_of_day(:remote_created_at).count
+    @posts_per_dow = @ppdow_posts.group_by_day_of_week(:remote_created_at).count.
+      map { |k,v| [dow_to_name(k), v] }
+    @posts_per_hod = @pphod_posts.group_by_hour_of_day(:remote_created_at).count
   end
 
   private
