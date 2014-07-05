571f1af0f571b0357f2a71e2dba4bef7bd408b3e
Luka Novsak <lnovsak@gmail.com>
2014-07-05 23:20:32 +0200

    add attributes to posts index for sorting

--- app/indices/post_index.rb
+++ app/indices/post_index.rb
@@ -1,6 +1,11 @@
 ThinkingSphinx::Index.define :post, with: :real_time do
   indexes body
+
+  has topic_id, type: :integer
+  has user_id, type: :integer
   has remote_created_at, type: :timestamp
+  has remote_id, type: :integer
+
   set_property enable_star: 1
   set_property min_infix_len: 3
   set_property dict: :keywords
