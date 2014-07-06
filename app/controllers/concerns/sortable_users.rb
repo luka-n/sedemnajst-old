1b3c9c47b603d3cbafda0491e2132ba8284044f7
Luka Novsak <lnovsak@gmail.com>
2014-07-06 10:00:03 +0200

    fix indent

--- app/controllers/concerns/sortable_users.rb
+++ app/controllers/concerns/sortable_users.rb
@@ -6,14 +6,14 @@ module SortableUsers
   def map_sort_key(key, default)
     map = {id: "id",
            id_desc: "id DESC",
-            name: "name",
-            name_desc: "name DESC",
-            remote_id: "remote_id",
-            remote_id_desc: "remote_id DESC",
-            posts_count: "posts_count",
-            posts_count_desc: "posts_count DESC",
-            topics_count: "topics_count",
-            topics_count_desc: "topics_count DESC"}
+           name: "name",
+           name_desc: "name DESC",
+           remote_id: "remote_id",
+           remote_id_desc: "remote_id DESC",
+           posts_count: "posts_count",
+           posts_count_desc: "posts_count DESC",
+           topics_count: "topics_count",
+           topics_count_desc: "topics_count DESC"}
     map[key.try(:to_sym)] || map[default.to_sym]
   end
 end
