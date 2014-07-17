0ed544f17bceebc0b191d2155e4f91fbe8bd1552
Luka Novsak <lnovsak@gmail.com>
2014-07-18 00:27:09 +0200

    eh

--- db/migrate/20140717214142_timestamps_for_all.rb
+++ db/migrate/20140717214142_timestamps_for_all.rb
@@ -1,18 +1,12 @@
 class TimestampsForAll < ActiveRecord::Migration
   def up
-    add_column :users, :created_at, :timestamp, null: false
-    add_column :users, :updated_at, :timestamp, null: false
-    add_column :posts, :created_at, :timestamp, null: false
-    add_column :posts, :updated_at, :timestamp, null: false
-    add_column :topics, :created_at, :timestamp, null: false
-    add_column :topics, :updated_at, :timestamp, null: false
     execute <<-SQL
-      ALTER TABLE users ALTER COLUMN created_at SET DEFAULT now();
-      ALTER TABLE users ALTER COLUMN updated_at SET DEFAULT now();
-      ALTER TABLE posts ALTER COLUMN created_at SET DEFAULT now();
-      ALTER TABLE posts ALTER COLUMN updated_at SET DEFAULT now();
-      ALTER TABLE topics ALTER COLUMN created_at SET DEFAULT now();
-      ALTER TABLE topics ALTER COLUMN updated_at SET DEFAULT now();
+      ALTER TABLE users ADD COLUMN created_at timestamptz NOT NULL DEFAULT now();
+      ALTER TABLE users ADD COLUMN updated_at timestamptz NOT NULL DEFAULT now();
+      ALTER TABLE posts ADD COLUMN created_at timestamptz NOT NULL DEFAULT now();
+      ALTER TABLE posts ADD COLUMN updated_at timestamptz NOT NULL DEFAULT now();
+      ALTER TABLE topics ADD COLUMN created_at timestamptz NOT NULL DEFAULT now();
+      ALTER TABLE topics ADD COLUMN updated_at timestamptz NOT NULL DEFAULT now();
     SQL
   end
 
